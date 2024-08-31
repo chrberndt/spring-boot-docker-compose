ba84e8f41f1a8365dd17288f68a5571865b9f273
Christian Berndt <christian.berndt@liferay.com>
2024-08-31 18:10:04 +0200

    chore: (re)create table and insert data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Countries(id, name) VALUES (1, 'Austria');
+INSERT INTO Countries(id, name) VALUES (2, 'Germany');
+INSERT INTO Countries(id, name) VALUES (3, 'Switzerland');
+INSERT INTO Countries(id, name) VALUES (4, 'Belgium');
+INSERT INTO Countries(id, name) VALUES (5, 'Luxemburg');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS Countries;
+
+CREATE TABLE Countries
+(
+    id  int,
+    name  varchar(255)
+);
