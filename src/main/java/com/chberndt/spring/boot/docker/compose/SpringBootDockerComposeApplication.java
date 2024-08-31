c2b8c29d096e80a3e8f9fef379d26af78c59e542
Christian Berndt <christian.berndt@liferay.com>
2024-08-31 15:02:18 +0200

    chore: initial checkin.

---
+++ src/main/java/com/chberndt/spring/boot/docker/compose/SpringBootDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.chberndt.spring.boot.docker.compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerComposeApplication.class, args);
+    }
+
+}
